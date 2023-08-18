0aa4c7295bda3948dd22f90c577af95a1aadcb3d
lucas.almeida <lucas.almeida@willbank.com.br>
2023-08-18 17:01:54 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <form @submit.prevent="generatePDF">
+      <label for="nome">Nome:</label>
+      <input type="text" id="nome" v-model="nome" required />
+      
+      <label for="email">Email:</label>
+      <input type="email" id="email" v-model="email" required />
+
+      <button type="submit">Gerar PDF</button>
+    </form>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import jsPDF from "jspdf";
+import logoImg from "@/assets/logo.png"; // Importe sua imagem aqui
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      nome: "", // Valor padrão
+      email: "", // Valor padrão
+    };
+  },
+  methods: {
+    async generatePDF() {
+      const pdf = new jsPDF();
+
+      // Carrega a imagem
+      const imgBlob = await this.getBlobFromImage(logoImg);
+
+      // Cria uma URL do Blob da imagem
+      const imgBlobUrl = URL.createObjectURL(imgBlob);
+
+      // Insere a imagem no PDF
+      pdf.addImage(imgBlobUrl, "PNG", 10, 10, 40, 40); // Posição e tamanho da imagem
+
+      // Insere o texto do nome e email
+      const texto = `Nome: ${this.nome}\nEmail: ${this.email}`;
+      pdf.text(texto, 10, 60);
+
+      // Salva o PDF em um Blob
+      const pdfBlob = pdf.output("blob");
+
+      // Cria uma URL do Blob e abre em uma nova guia
+      const pdfUrl = URL.createObjectURL(pdfBlob);
+      window.open(pdfUrl, "_blank");
+    },
+
+    getBlobFromImage(image) {
+      return new Promise((resolve) => {
+        fetch(image)
+          .then((response) => response.blob())
+          .then((blob) => resolve(blob));
+      });
+    },
+  },
+};
 </script>
 
 <style>
